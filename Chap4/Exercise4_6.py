99d66f487aaf0c77c878259371345566e65827f4
Jiazhen Fan <jfan17@jhu.edu>
2017-01-17 10:19:34 +0800

    Chap 4 Added

---
+++ Chap4/Exercise4_6.py
@@ -0,0 +1,29 @@
+"""
+Exercise 4.6 Rewrite your pay computation with time-and-a-half for overtime and 
+create a function called computepay which takes two parameters (hours and rate).
+Enter Hours: 45 Enter Rate: 10 Pay: 475.0
+"""
+
+import sys
+
+try: 
+    hours = float(raw_input('Enter Hours: '))
+    rate = float(raw_input('Enter Rate: '))
+
+    
+except:
+    print 'Please enter a number'
+    sys.exit()
+    
+
+
+def total_pay(hours, rate):
+    if hours > 40: 
+        pay = (hours - 40) * rate * 1.5 + 40 * rate
+    else:
+        pay = hours * rate
+    return pay
+
+
+print 'Pay: ', total_pay(hours, rate)
+
---
+++ Chap4/Exercise4_7.py
@@ -0,0 +1,43 @@
+"""
+Exercise 4.7 Rewrite the grade program from the previous chapter using a 
+func- tion called computegrade that takes a score as its parameter and returns 
+a grade as a string.
+Score Grade >0.9 A >0.8 B >0.7 C >0.6 D <=0.6 F
+Program Execution: 
+Enter score: 0.95 A
+Enter score: perfect Bad score
+Enter score: 10.0 Bad score
+Enter score: 0.75 C
+Enter score: 0.5 F
+Run the program repeatedly to test the various different values for input.
+"""
+def computegrade(score):
+    if score >= 0 and score <= 1.0:
+        if score > 0.9:
+            grade = 'A'
+        elif score > 0.8:
+            grade = 'B'
+        elif score > 0.7:
+            grade = 'C'
+        elif score > 0.6:
+            grade = 'D'
+        else:
+            grade = 'F'
+        print grade
+    else:
+        print 'Bad score'
+
+while True:
+    score = raw_input('Enter score: ')
+
+    try:
+        score = float(score)
+
+    except:
+        print 'Bad score'
+        break
+    
+    computegrade(score) 
+    
+
+
